084aaa026d49211ca8873648ef54e9a8b1c466ec
sobryan <stobryan@gmail.com>
2023-01-12 00:07:06 -0600

    Replace spring retry with resilience4j, tuning circuit breaker

--- ActorService/src/main/java/com/example/demo/clients/MovieClient.java
+++ ActorService/src/main/java/com/example/demo/clients/MovieClient.java
@@ -1,15 +1,17 @@
 package com.example.demo.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.domain.Movie;
 
+import io.github.resilience4j.retry.annotation.Retry;
+
+@Retry(name = "MovieService")
 @FeignClient(name = "MovieService")
 public interface MovieClient {
 
-	@Retryable(maxAttempts = 3 )
+//	@Retryable(maxAttempts = 1 )
 	@GetMapping(value="/movie")
 	Movie findMovieByName();
 	
